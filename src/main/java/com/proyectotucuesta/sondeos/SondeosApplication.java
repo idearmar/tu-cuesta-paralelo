a986c6151e67959e8ae214339833fd7f80cf4b67
idearmar <62974969+idearmar@users.noreply.github.com>
2021-11-17 06:55:38 -0500

    Se añade modelo equipo

---
+++ src/main/java/com/proyectotucuesta/sondeos/SondeosApplication.java
@@ -0,0 +1,13 @@
+package com.proyectotucuesta.sondeos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SondeosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SondeosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectotucuesta/sondeos/SondeosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectotucuesta.sondeos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SondeosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
